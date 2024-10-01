3050308446a5428435108769d32e62b33429f155
jamilylinda <143099405+milylinda@users.noreply.github.com>
2024-10-01 16:33:02 -0300

    Add files via upload

---
+++ informacoesGlobais.js
@@ -0,0 +1,6 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json';
+
+async function visualizarInformacoesGlobais(){
+    const res = await fetch(url)
+}
+    
